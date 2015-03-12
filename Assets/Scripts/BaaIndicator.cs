e7bec54663f11429c686f19dfb5b3cbcb91cc65a
Tom Cole <tom@tommakesgames.com>
2015-03-12 18:23:55 +0000

    Added beginnings of response dynamic

---
+++ Assets/Scripts/BaaIndicator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaaIndicator : MonoBehaviour {
+
+    public GameObject sheep;
+
+
+	// Use this for initialization
+	void Start () {
+
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (sheep.audio.isPlaying)
+            sheep.renderer.material.color = Color.black;
+
+        if (!sheep.audio.isPlaying)
+            sheep.renderer.material.color = Color.white;
+	
+	}
+}
---
+++ Assets/Scripts/ResponseIndicator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResponseIndicator : MonoBehaviour {
+
+    public GameObject sheep;
+    public GameObject responder;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (responder.audio.isPlaying)
+            sheep.renderer.material.color = Color.red;
+
+        if (!responder.audio.isPlaying)
+            sheep.renderer.material.color = Color.white;
+	
+	}
+}
